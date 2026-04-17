e60ed02cca7e0f13bda7ebafae2b08f87d6971b7
calvinwijaya <calvin.wijaya@mail.ugm.ac.id>
2026-04-17 16:01:51 +0700

    Update admin

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const ADMIN_EMAILS = [
     "calvin.wijaya@mail.ugm.ac.id",
     "cecep.pratama@ugm.ac.id",
-    "natasha.dev2003@mail.ugm.ac.id"
+    "natasha.dev2003@mail.ugm.ac.id",
+    "muhammad.rakha1803@mail.ugm.ac.id",
+    "krishna.dut2002@mail.ugm.ac.id"
 ];
 
 const DOSEN_MAP = {
